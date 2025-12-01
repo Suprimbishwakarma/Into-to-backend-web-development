ffbc1a10f98422d1512af93852a21049c3550b2e
suprimbishwakarma <suprimbishwakarma2002@gmail.com>
2025-12-01 16:34:40 +0545

    minor issue fixed

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const connectioInstance = await mongoose.connect(
+    const connectionInstance = await mongoose.connect(
       `${process.env.DATABASE_URI}`
     );
     console.log(
-      `Connected to the database! ${connectioInstance.connection.host}`
+      `Connected to the database! ${connectionInstance.connection.host}`
     );
   } catch (error) {
     console.log("Failed database connection");
